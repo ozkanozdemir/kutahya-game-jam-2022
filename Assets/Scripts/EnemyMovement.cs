9940bde2f315668b307e24be2a1b83c4cd87b697
ozkanozdemir <ozkan.ozdemir4114@gmail.com>
2022-05-14 12:03:31 +0300

    ali çavuş ve enemy'nin boyutu arttırıldı

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -8,23 +8,37 @@ public class EnemyMovement : MonoBehaviour
     [SerializeField] private float moveSpeed = 1f;
 
     private Rigidbody2D _rigidbody;
+    private Animator _animator;
     
     // Start is called before the first frame update
     private void Start()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     private void Update()
     {
         _rigidbody.velocity = new Vector2(moveSpeed, 0f);
+        
+        if (Mathf.Abs(_rigidbody.velocity.x) > 0)
+        {
+            _animator.SetInteger("Animate", 2);
+        }
+        else
+        {
+            _animator.SetInteger("Animate", 0);
+        }
     }
 
     private void OnTriggerExit2D(Collider2D other)
     {
-        moveSpeed = -moveSpeed;
-        FlipEnemyFacing();
+        if (other.tag.Equals("Wall"))
+        {
+            moveSpeed = -moveSpeed;
+            FlipEnemyFacing();   
+        }
     }
 
     private void FlipEnemyFacing()
